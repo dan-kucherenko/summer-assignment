e32956727e0ae51f8a74c8337d930752d987354a
dan_kucherenko <daniil.ukraine@gmail.com>
2022-06-08 00:27:33 +0300

    Creating GUI

--- BuildAGraph.py
+++ BuildAGraph.py
@@ -1,11 +1,31 @@
 import numpy as np
-import matplotlib.pyplot as plot
+import matplotlib.pyplot as plt
+import PySimpleGUI as psg
 
-a = int(input("Enter the value for parameter 'a': "))
-print("Entered values is: ", a)
 
-fi = np.linspace(0, 2 * np.pi)
-r = pow(2 * pow(a, 2) * np.cos(2 * fi))
+def build_gui():
+    layout = [
+        [psg.Text("Enter the value for parameter 'a': "), psg.InputText()],
+        [psg.Save()],
+        [psg.Submit(), psg.Cancel()]]
 
-x = np.array(r * np.cos(fi))
-y = np.array(r * np.sin(fi))
+    window = psg.Window("Лемніската", layout)
+
+
+def build_graph():
+    a = int(input("Enter the value for parameter 'a': "))
+    print("Entered values is: ", a)
+
+    fi = np.linspace(0, 2 * np.pi, 1000)
+    r_sq = pow(2 * pow(a, 2) * np.cos(2 * fi), 2)
+
+    x = np.array(np.sqrt(r_sq) * np.cos(fi))
+    y = np.array(np.sqrt(r_sq) * np.sin(fi))
+
+    plt.plot(x, y, color='blue')
+    plt.title("Лемніската")
+    plt.grid()
+    plt.show()
+
+
+build_graph()
