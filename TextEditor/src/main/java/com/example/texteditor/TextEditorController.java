4515fb8f240cb3e30b722b8f8af835b03ea2abae
Daniil Kucherenko <daniil.ukraine@gmail.com>
2022-06-13 14:30:09 +0300

    Refactored some files

--- TextEditor/src/main/java/com/example/texteditor/TextEditorController.java
+++ TextEditor/src/main/java/com/example/texteditor/TextEditorController.java
@@ -3,7 +3,7 @@ package com.example.texteditor;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
-public class HelloController {
+public class TextEditorController {
     @FXML
     private Label welcomeText;
 
