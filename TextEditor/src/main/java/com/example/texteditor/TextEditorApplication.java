73a428272d65e3a84ffa7d1cf0ae07bbcbc999cf
Daniil Kucherenko <daniil.ukraine@gmail.com>
2022-06-13 15:19:23 +0300

    Handler for button "open" in file menu added

--- TextEditor/src/main/java/com/example/texteditor/TextEditorApplication.java
+++ TextEditor/src/main/java/com/example/texteditor/TextEditorApplication.java
@@ -12,7 +12,7 @@ public class TextEditorApplication extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(TextEditorApplication.class.getResource("text-editor-gui.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        stage.setTitle("Text Editor");
         stage.setScene(scene);
         stage.show();
     }
--- TextEditor/src/main/java/com/example/texteditor/TextEditorController.java
+++ TextEditor/src/main/java/com/example/texteditor/TextEditorController.java
@@ -1,14 +1,60 @@
 package com.example.texteditor;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.stage.FileChooser;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class TextEditorController {
     @FXML
-    private Label welcomeText;
+    public TextArea textArea;
+
+    //    ---------------File menu controller--------
+    @FXML
+    private void onNewClicked() {
+    }
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private void onOpenClicked() {
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Open File");
+        fileChooser.getExtensionFilters().add
+                (new FileChooser.ExtensionFilter("All Text Files", "*.txt", "*.html"));
+        fileChooser.getExtensionFilters().add
+                (new FileChooser.ExtensionFilter("All Files", "*"));
+        File file = fileChooser.showOpenDialog(textArea.getScene().getWindow());
+        if (file == null)
+            return;
+        textArea.clear();
+        try {
+            BufferedReader bufferedReader = new BufferedReader(new FileReader(file));
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                textArea.appendText(line + '\n');
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @FXML
+    private void onSaveClicked() {
+    }
+
+    @FXML
+    private void onSaveAsClicked() {
+    }
+
+    @FXML
+    private void onCloseClicked() {
+    }
+
+    @FXML
+    private void onExitClicked() {
+        System.exit(0);
     }
 }
