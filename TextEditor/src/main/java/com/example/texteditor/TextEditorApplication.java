ac88d32c831f6fde1be98e1c26a01b4c91dcd7ac
Daniil Kucherenko <daniil.ukraine@gmail.com>
2022-06-14 10:31:59 +0300

    Resizable window added

--- TextEditor/src/main/java/com/example/texteditor/TextEditorApplication.java
+++ TextEditor/src/main/java/com/example/texteditor/TextEditorApplication.java
@@ -11,7 +11,7 @@ public class TextEditorApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(TextEditorApplication.class.getResource("text-editor-gui.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        Scene scene = new Scene(fxmlLoader.load(), 800, 600);
         stage.setTitle("Text Editor");
         stage.setScene(scene);
         stage.show();
