da1f9864c43d50c286415110819b329a22c05055
dan_kucherenko <daniil.ukraine@gmail.com>
2022-06-17 11:59:54 +0300

    Number of dots were replaced with step of dots value

--- BuildGraph/BuildAGraph.py
+++ BuildGraph/BuildAGraph.py
@@ -3,13 +3,13 @@ import matplotlib.pyplot as pypt
 import PySimpleGUI as psg
 
 
-def build_gui(): # creating a GUI
+def build_gui():  # creating a GUI
     psg.theme("DarkAmber")
     layout = [
         [psg.Text("Value for parameter a: "), psg.InputText()],
         [psg.Text("Min fi value: "), psg.InputText()],
         [psg.Text("Max fi value: "), psg.InputText()],
-        [psg.Text("Number of dots: "), psg.InputText()],
+        [psg.Text("Step: "), psg.InputText()],
         [psg.Save(), psg.Button("Calculate function"), psg.Button("Cancel")]]
     return layout
 
@@ -26,23 +26,23 @@ def build_graph():
             break
         elif events == "Calculate function":
             try:
-                pypt.clf() # clear the window with graph in case any graph was already there
+                pypt.clf()  # clear the window with graph in case any graph was already there
                 # initialise values
                 a = int(val[0])
                 min_fi_val = float(val[1])
                 max_fi_val = float(val[2])
-                dots_num = int(val[3])
+                step = float(val[3])
             except:
                 psg.popup("Error in inserted data")
                 break
 
-            fi = np.linspace(min_fi_val, max_fi_val, dots_num)
+            fi = np.linspace(min_fi_val, max_fi_val, int((max_fi_val - min_fi_val) / step))
             r_sq = 2 * (a ** 2) * np.cos(2 * fi)
             r_sq[r_sq < 0] = 0
             x = np.sqrt(r_sq) * np.cos(fi)
             y = np.sqrt(r_sq) * np.sin(fi)
 
-            pypt.plot(x, y, color="blue") # plot a graph
+            pypt.plot(x, y, color="blue")  # plot a graph
             # add axis labels and title of the graph
             pypt.xlabel("X axis")
             pypt.ylabel("Y axis")
